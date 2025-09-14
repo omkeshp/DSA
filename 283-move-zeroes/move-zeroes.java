3041d77c31d34e1822fdf3a2715731d8ac9cc1f3
omkeshp <112849776+omkeshp@users.noreply.github.com>
2025-09-14 21:10:32 +0530

    Time: 2 ms (79.46%) | Memory: 46 MB (79.18%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -3,6 +3,9 @@ class Solution {
         int n = nums.length;
         int nonZeroIndex = 0;
         int [] temp = new int [n];
+        if(n == 1){
+            System.out.println(nums[0]);
+        }
 
         for(int i = 0; i< n; i++){
             if(nums[i]!=0){
